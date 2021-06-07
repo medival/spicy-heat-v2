0fb33369011993402a82462b6f47034fb962460a
Adi Purnomo <adipurnomo811@gmail.com>
2021-06-07 21:02:46 +0700

    update: Change variable name

--- src/scripts/data/resto-source.js
+++ src/scripts/data/resto-source.js
@@ -1,13 +1,13 @@
 import API_ENDPOINT from '../global/api-endpoint';
 
 class RestoDBSource {
-	static async restoFoods() {
+	static async infoResto() {
 		const response = await fetch(API_ENDPOINT.HOME);
 		const responseJson = await response.json()
 		return responseJson.restaurants;
 	}
 	
-	static async detailFoods(id) {
+	static async detailResto(id) {
 		const response = await fetch(API_ENDPOINT.DETAIL(id));
 		return response.json();
 	}
