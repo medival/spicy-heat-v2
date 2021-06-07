2b2d5707f0b2ee6c336afc10be36778ad53f95fe
Adi Purnomo <adipurnomo811@gmail.com>
2021-06-07 21:09:05 +0700

    update: Add function after render

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,12 +1,21 @@
+import UrlParser from '../../routes/url-parser';
+import RestoDBSource from '../../data/resto-source';
+import { createHeroTemplate } from '../templates/template-creator';
+
 const Detail = {
   async render() {
-    return `
-      <h2>Detail Page</h2>
+     return `
+      <div id="resto" class="resto"> </div>
     `;
   },
  
   async afterRender() {
-    // Fungsi ini akan dipanggil setelah render()
+    const url = UrlParser.parseActiveUrlWithoutCombiner();
+    const detailResto = await RestoDBSource.detailResto(url.id);
+
+    console.log(detailResto);
+    const restoContainer = document.querySelector('#resto');
+    restoContainer.innerHTML +=  createHeroTemplate(detailResto);
   },
 };
  
