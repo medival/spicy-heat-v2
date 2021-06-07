ac1ad2a6eb6824bb66f348af6f5adedaedb6b406
medival <dev.adipurnomo@gmail.com>
2021-06-07 13:56:17 +0700

    code: Refactor code from spicy-heat

---
+++ src/scripts/views/app.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-underscore-dangle */
+
+import DrawerInitiator from '../utils/drawer-initiator';
+
+class App {
+  constructor({ button, drawer, content }) {
+    this._button = button;
+    this._drawer = drawer;
+    this._content = content;
+
+    this._initialAppShell();
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  _initialAppShell() {
+    DrawerInitiator.init({
+      button: this._button,
+      drawer: this._drawer,
+      content: this._content,
+    });
+
+    // other component
+  }
+}
+
+export default App;
