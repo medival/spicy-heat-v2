3ee9801ad5ae2164ee0c31c03169df06128a386a
medival <dev.adipurnomo@gmail.com>
2021-06-07 14:53:35 +0700

    update: Add renderPage function

--- src/scripts/views/app.js
+++ src/scripts/views/app.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-underscore-dangle */
 
 import DrawerInitiator from '../utils/drawer-initiator';
+import UrlParser from '../routes/url-parser';
+import routes from '../routes/routes';
 
 class App {
   constructor({ button, drawer, content }) {
@@ -21,6 +23,13 @@ class App {
 
     // other component
   }
-}
+  
+  async renderPage() {
+    const url = UrlParser.parseActiveUrlWithCombiner();
+    const page = routes[url];
+    this._content.innerHTML = await page.render();
+    await page.afterRender();
+  }
+};
 
 export default App;
